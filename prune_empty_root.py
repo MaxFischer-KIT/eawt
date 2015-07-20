d4c9a5c3f102b12ea82591d312b07e0e68444c28
Max Fischer <max.fischer@kit.edu>
2015-07-20 14:25:00 +0200

    added tool for removing empty root files

---
+++ prune_empty_root.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python
+import ROOT
+import argparse
+import glob
+import itertools
+import os
+
+ROOT.gErrorIgnoreLevel = ROOT.kError
+
+def file_has_events(file_path, branch_name):
+    try:
+        data = ROOT.TFile(file_path)
+        return data.Get(branch_name).GetEntries() > 0
+    except AttributeError:
+        # some data structure is missing
+        return False
+
+CLI = argparse.ArgumentParser(
+    description="Remove empty ROOT files",
+    epilog="This scripts inspects files by opening them and checking whether"
+           "the Branch BRANCH_NAME has any entries."
+)
+CLI.add_argument(
+    "files",
+    nargs="*",
+    help="Files to check"
+)
+CLI.add_argument(
+    "--branch-name",
+    default="Events",
+    help="Branch name inside the files containing relevant content",
+)
+CLI.add_argument(
+    "-d",
+    "--dry-run",
+    action="store_true",
+    help="Do not actually delete anything, just report",
+)
+
+if __name__ == "__main__":
+    args = CLI.parse_args()
+    for file_path in itertools.chain(*(glob.glob(cand) for cand in args.files)):
+        if not file_has_events(file_path, args.branch_name):
+            print "rm", file_path
+            if not args.dry_run:
+                os.unlink(file_path)
